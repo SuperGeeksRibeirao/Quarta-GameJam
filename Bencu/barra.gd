fbaf8910664b7010d74598d6cd6df9f1fa0bfc0d
SuperGeeksRibeirao <134536679+SuperGeeksRibeirao@users.noreply.github.com>
2023-06-07 15:12:51 -0300

    AI GOSTOSO ZE DA MANGA ENZO METE MAIS FRACO OSWALDOFUMANDODROGAS PARAAAAAAAAAAAAAAAAAAAAAA
    mds que história de amor eterno

--- Bencu/barra.gd
+++ Bencu/barra.gd
@@ -1,11 +1,13 @@
 extends KinematicBody2D
 
-export var speed = 400
+var speed = 500
+var movimento = Vector2()
 
 func _physics_process(delta):
-	var velocity = Vector2()
+	
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= speed
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += speed
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+		movimento.y = -speed
+	elif Input.is_action_pressed("ui_down"):
+		movimento.y = speed
+		
+	move_and_slide(movimento)
