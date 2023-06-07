ae5dfe391140e27a54f9e1d1483cdc9757f9f4fe
SuperGeeksRibeirao <134536679+SuperGeeksRibeirao@users.noreply.github.com>
2023-06-07 15:37:58 -0300

    Pedro

---
+++ Bencu/player.gd
@@ -0,0 +1,8 @@
+extends KinematicBody2D
+
+var velocidade
+var direccion = Vector2.ZERO
+
+func _physics_process(delta: float)->void:
+	direccion.y =Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up")
+	move_and_slide(direccion*velocidade)
