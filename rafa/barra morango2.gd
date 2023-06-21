e19caac1cfa611c79a2748e57695089c38923628
SuperGeeksRibeirao <134536679+SuperGeeksRibeirao@users.noreply.github.com>
2023-06-21 15:28:33 -0300

    ola

--- rafa/barra morango2.gd
+++ rafa/barra morango2.gd
@@ -1,13 +1,14 @@
 extends KinematicBody2D
 
-var movimento = Vector2()
 var speed = 500
+var movimento = Vector2()
 
 func _physics_process(delta):
 	
-	if Input.is_key_pressed(KEY_W):
+	if Input.is_action_pressed("ui_up"):
 		movimento.y = -speed
-	elif Input.is_key_pressed(KEY_S):
+	elif Input.is_action_pressed("ui_down"):
 		movimento.y = speed
-		
+	else:
+		movimento.y = speed - speed
 	move_and_slide(movimento)
