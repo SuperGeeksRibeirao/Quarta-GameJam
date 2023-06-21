ea26916c2967b82c338a86ff70783fb81d916b3d
SuperGeeksRibeirao <134536679+SuperGeeksRibeirao@users.noreply.github.com>
2023-06-21 15:27:50 -0300

    coisas legais e refinamento

--- rafa/boula.gd
+++ rafa/boula.gd
@@ -1,7 +1,8 @@
 extends Area2D
 var speed = Vector2(0,350)
 var random = RandomNumberGenerator.new()
-
+var PontoPlayer1 = 0
+var PontoPlayer2 = 0
 func _ready():
 	random.randomize()
 	var my_random_number = random.randf_range(-360.0,360.0)
@@ -18,3 +19,19 @@ func _on_boula_body_entered(body):
 		speed = -speed
 		rotation_degrees += rand_range(0,90)
 
+
+
+func _on_Area2D2_area_entered(area):
+	PontoPlayer1 = PontoPlayer1 + 1
+	global_position = Vector2(759,591)
+	rotation_degrees += random.randf_range(-360.0,360.0)
+	
+func _on_Area2D_area_entered(area):
+	PontoPlayer2 = PontoPlayer2 + 1
+	global_position = Vector2(759,591)
+	rotation_degrees += random.randf_range(-360.0,360.0)
+
+
+func _on_Area2D4_area_entered(area):
+	global_position = Vector2(759,591)
+	rotation_degrees += random.randf_range(-360.0,360.0)
